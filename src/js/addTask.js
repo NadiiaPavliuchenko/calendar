63b6091069668d2555c544124e109e873da396e8
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-15 14:54:49 +0200

    add styles for calendar controls

--- src/js/addTask.js
+++ src/js/addTask.js
@@ -5,10 +5,10 @@ export const setDateValue = (date) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
 
-  const dateString = `${year}-${month}-${day}T0${hours}:0${minutes}`;
+  const dateString = `${year}-${month}-${day}T${hours}:${minutes}`;
   refs.form.elements.date.value = dateString;
 };
 
